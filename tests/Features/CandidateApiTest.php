354cd14c78c2d337b5e152b6e77b53225a684717
sakhawathossensadi <newtonss919@gmail.com>
2022-09-03 16:04:35 +0600

    config TestCase

---
+++ tests/Features/CandidateApiTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Analyzen\Candidate\Tests\Features;
+
+use Analyzen\Candidate\Tests\TestCase;
+
+class CandidateApiTest extends TestCase
+{
+    public function tests_candidate_registration()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Analyzen\Candidate\Tests;
+
+use Analyzen\Candidate\Models\Candidate;
+use Illuminate\Foundation\Testing\WithFaker;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Laravel\Passport\PassportServiceProvider;
+use Analyzen\Candidate\ServiceProvider;
+
+class TestCase extends OrchestraTestCase
+{
+    use WithFaker;
+
+    protected $candidate;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadLaravelMigrations();
+        $this->artisan('migrate')->run();
+
+        $candidate = Candidate::factory()->create();
+        // dd($candidate);
+        $this->candidate = $candidate;
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return void
+     */
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $guards = $app['config']->get('auth.guards');
+
+        $guards['api'] = [
+            'driver' => 'token',
+            'provider' => 'users',
+            'hash' => false,
+        ];
+
+        $app['config']->set('auth.guards', $guards);
+
+        $app['config']->set('auth.defaults.guard', 'api');
+
+        $app['config']->set('auth.providers.users.model', Candidate::class);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            'Analyzen\\Candidate\\ServiceProvider',
+            'Analyzen\\Candidate\\AuthServiceProvider',
+            PassportServiceProvider::class,
+            ServiceProvider::class,
+        ];
+    }
+}
