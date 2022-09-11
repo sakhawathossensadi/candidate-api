d6c27b7cde55558e3a0314f7f819e96138a5c4ab
sakhawathossensadi <newtonss919@gmail.com>
2022-09-11 22:59:27 +0600

    functionality for quiz

--- src/Http/Controllers/CandidateController.php
+++ src/Http/Controllers/CandidateController.php
@@ -5,6 +5,8 @@ namespace Analyzen\Candidate\Http\Controllers;
 use Analyzen\Candidate\Http\Requests\CandidateRequest;
 use Analyzen\Candidate\Http\Resources\CandidateResource;
 use Analyzen\Candidate\Models\Candidate;
+use Analyzen\Candidate\Models\Question;
+use Illuminate\Http\Request;
 
 class CandidateController extends BaseController
 {
@@ -18,5 +20,35 @@ class CandidateController extends BaseController
         $candidate = Candidate::create($userData);
 
         return response(new CandidateResource($candidate), 201);
+    }
+
+    public function quiz(Request $request)
+    {
+        $candidate = $request->user();
+        $candidateAnswer = $request->answer;
+        $ans = [];
+        $count = 0;
+
+        if ($candidate->status == 0) {
+            return response([
+                'message' => "You are not eligible for quiz",
+            ]);
+        }
+
+        $questions = Question::all();
+
+        foreach ($questions as $question) {
+            array_push($ans, $question->answer);
+        }
+
+        for ($i = 0; $i < $questions->count(); $i++) {
+            if ($ans[$i] == $candidateAnswer[$i]) {
+                $count++;
+            }
+        }
+
+        return response([
+            'mark' => "Obtained mark : " . (string) $count,
+        ]);
     }
 }
